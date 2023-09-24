7abc3a3f38f513b0958c5489ff79c484997ec733
agrofrank <franco.mangone@agrotoken.io>
2023-09-24 18:51:20 -0300

    Instantiates etf, and creates hook

--- context/etf.tsx
+++ context/etf.tsx
@@ -1,17 +1,39 @@
-import { createContext, useEffect, useRef } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { Etf } from '@ideallabs/etf.js';
 
-const EtfContext = createContext(null);
+interface EtfContextValues {
+	api: Etf<{}>
+	latestSlot: any
+}
+
+const EtfContext = createContext<EtfContextValues>(null);
 
 type EtfProviderProps = {
 	children?: React.ReactNode;
 };
 
+// Hook for easy access from anywhere in the component tree
+export const useEtf = (): EtfContextValues => {
+	return useContext(EtfContext)
+}
+
 export const EtfProvider: React.FC<EtfProviderProps> = (props) => {
 	const { children } = props;
 
-	const etf = useRef();
+	const api = useRef(new Etf());
+  const [latestSlot, setLatestSlot] = useState(null)
 
-	useEffect(() => {}, []);
+	useEffect(() => {
+		const setup = async () => {
+      await api.current.init()
 
-	return <EtfContext.Provider value={etf}>{children}</EtfContext.Provider>;
+      api.current.eventEmitter.on('blockHeader', () => {
+        setLatestSlot(api.current.latestSlot)
+      })
+		}
+
+		setup()
+	}, []);
+
+	return <EtfContext.Provider value={{ api: api.current, latestSlot }}>{children}</EtfContext.Provider>;
 };
