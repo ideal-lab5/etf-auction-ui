60338f2fed9671d68da3b846b68d74ed879798a7
driemworks <tonyrriemer@gmail.com>
2023-10-24 13:14:34 -0500

    bump etf.js, update next config

--- next.config.cjs
+++ next.config.cjs
@@ -3,6 +3,8 @@ const nextConfig = {
   output: 'export',
   trailingSlash: false,
   assetPrefix: '.',
+  experiments: { asyncWebAssembly: true },
+  output: webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
 }
 
 module.exports = nextConfig
